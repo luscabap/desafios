daa9df904d040bab6b85edf20e39d91d1d53e930
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-16 18:43:56 -0300

    feat: base Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,36 @@
 import { useNavigate } from "react-router-dom"
+import { Logo } from "../Logo";
+import { desafios } from "../../data/desafios";
+import { useState } from "react";
 
 export const Header = () => {
   const navigate = useNavigate();
+  const [desafiosIsOpen, setDesafiosIsOpen] = useState(false);
+
+  const toggleDesafioIsOpen = () => {
+    setDesafiosIsOpen(pv => !pv)
+  }
+
+  const clickedDesafioItem = (slug: string) => {
+    navigate(`/desafio/${slug}`)
+    setDesafiosIsOpen(false)
+  }
 
   return (
-    <header className="bg-slate-600 w-full flex items-center justify-evenly">
-      <h1>DESAFIO PARA DEVS</h1>
-      <nav>
-        <ul><li onClick={() => navigate("/")} className="cursor-pointer">Home</li></ul>
+    <header className="bg-slate-600 w-full flex items-center justify-between px-2 py-1">
+      <Logo />
+      <button onClick={() => navigate("/")} className="cursor-pointer">Home</button>
+      <nav className="relative text-right w-1/6">
+        <h3 onClick={toggleDesafioIsOpen} className="cursor-pointer text-center">Desafios</h3>
+        {desafiosIsOpen && (
+          <ul className="bg-slate-700 py-4 px-3 absolute mt-6 w-80 flex flex-col items-start justify-center gap-4 rounded-b-lg">
+            {desafios.map(desafio => (
+              <li onClick={() => clickedDesafioItem(desafio.slug)} className="cursor-pointer">{desafio.nome}
+              </li>
+            ))}
+          </ul>
+        )}
+
       </nav>
     </header>
   )
