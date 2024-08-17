88b7997819a3e025c1b5ffcbe223c8b0a845b1c5
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-17 19:37:42 -0300

    feat: SobreDesafio animation

--- src/components/SobreDesafio/index.tsx
+++ src/components/SobreDesafio/index.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { useState } from "react";
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import { AnimatePresence, motion } from "framer-motion";
 
 
 type TSobreDesafio = {
@@ -23,19 +24,27 @@ export const SobreDesafio = ({ descricao, slug, nome }: TSobreDesafio) => {
         <h2>{nome}</h2>
         { descIsOpen ? <IoMdArrowDropup size={22}/> : <IoMdArrowDropdown size={22}/>}
       </div>
-      {descIsOpen && (
-        <div className="flex flex-col gap-4 items-start justify-start">
-          <span className="text-slate-500 font-bold">O QUE FAZER?</span>
-          <p>{descricao}</p>
-          <button
-            onClick={() => navigate(`/desafio/${slug}`)}
-            className="flex items-center justify-center gap-2 text-slate-600 hover:text-slate-400"
+      <AnimatePresence>
+        {descIsOpen && (
+          <motion.div 
+            className="flex flex-col gap-4 items-start justify-start origin-top"
+            initial={{ scaleY: 0, opacity: 0, }}
+            animate={{ scaleY: 1, opacity: 1 }}
+            exit={{ scaleY: 0, opacity: 0 }}
+            transition={{ duration: 0.5 }}
           >
-            <span>Ir pra desafio</span>
-            <FaArrowRightLong size={22}/>
-          </button>
-        </div>
-      )}
+            <span className="text-slate-500 font-bold">O QUE FAZER?</span>
+            <p>{descricao}</p>
+            <button
+              onClick={() => navigate(`/desafio/${slug}`)}
+              className="flex items-center justify-center gap-2 text-slate-600 hover:text-slate-400"
+            >
+              <span>Ir pra desafio</span>
+              <FaArrowRightLong size={22}/>
+            </button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
