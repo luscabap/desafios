a4240c0d751caf1dba9167f47d6e153811caa0b2
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-17 19:41:28 -0300

    feat: DesafioPage animation

--- src/pages/DesafioPage/index.tsx
+++ src/pages/DesafioPage/index.tsx
@@ -3,6 +3,7 @@ import { desafios } from "../../data/desafios";
 import { RecordResolucoes } from "../../components/Resolucao/RecordResolucoes";
 import { TitleSection } from "../../components/TitleSection";
 import { IoIosArrowBack } from "react-icons/io";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const DesafioPage = () => {
   const params = useParams();
@@ -10,19 +11,29 @@ export const DesafioPage = () => {
 
   const desafio = desafios.find(desafio => desafio.slug === params.slugDesafio);
   return (
-    desafio && 
-    <div className="my-5 flex flex-col items-center justify-center gap-20 w-1/2">
-        <TitleSection text={desafio.nome} contrast={true} size="large"/>
-        <div className="w-full">
-          {
-            RecordResolucoes[desafio.resolucao]
-          }
-        </div>
+    desafio &&
+    <AnimatePresence>
+      <motion.div className="my-5 flex flex-col items-center justify-center gap-20 w-1/2">
+          <TitleSection text={desafio.nome} contrast={true} size="large"/>
+          <motion.div 
+            className="w-full"
+            initial={{ x: -150 }}
+            whileInView={{ x: 0 }}
+          >
+            {
+              RecordResolucoes[desafio.resolucao]
+            }
+          </motion.div>
 
-        <button className="text-slate-500 flex items-center justify-center w-full" onClick={() => navigate("/")}>
-          <IoIosArrowBack />
-          <span>Voltar a HomePage</span>
-        </button>
-    </div>
+          <motion.button 
+            className="text-slate-500 flex items-center justify-center w-full" onClick={() => navigate("/")}
+            initial={{ x: 100 }}
+            whileInView={{ x: 0 }}
+          >
+            <IoIosArrowBack />
+            <span>Voltar a HomePage</span>
+          </motion.button>
+      </motion.div>
+    </AnimatePresence>
   )
 };
