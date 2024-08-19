bba507a114be2eddc362a21f80d677b8d7d8f51d
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-19 17:16:46 -0300

    feat: componente FigurasPlanas

--- src/components/Calculos/FigurasPlanas/Triangulo.tsx
+++ src/components/Calculos/FigurasPlanas/Triangulo.tsx
@@ -11,12 +11,15 @@ type TValoresTriangulo = {
   hipo: null | number,
 }
 
+
+
 export const CalculoTriangulo = () => {
   const [valoresArea, setValoresArea] = useState<TValoresTriangulo>({
     base: null,
     altura: null,
     hipo: null
   })
+
   const [resultados, setResultados] = useState<TResultados>({
     area: null,
     perimetro: null
@@ -35,8 +38,19 @@ export const CalculoTriangulo = () => {
     })
   }
 
+  const calcularPerimetro = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const perimetro = (valoresArea.altura! + valoresArea.base! + valoresArea.hipo!);
+
+    setResultados({
+      ...resultados,
+      perimetro: `O valor do perímetro é: ${perimetro.toString()}`
+    })
+  }
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center gap-12">
       <form onSubmit={calcularArea} className="text-white flex flex-col items-center justify-center gap-4">
         <h3 className="text-slate-500 text-lg">Cálculo da Área</h3>
         <div className="flex flex-col items-start justify-center gap-2">
@@ -72,12 +86,53 @@ export const CalculoTriangulo = () => {
           <h4 className="">{resultados.area}</h4>
         ) }
       </form>
-      <form>
-        <h3>Perímetro</h3>
-        <input type="number" placeholder="Digite o cateto adjacente"/>
-        <input type="number" placeholder="Digite o cateto oposto"/>
-        <input type="number" placeholder="Digite a hipotenusa"/>
-        <button type="submit">Calcular perímetro</button>
+
+      <form onSubmit={calcularPerimetro} className="text-white flex flex-col items-center justify-center gap-4">
+        <h3 className="text-slate-500 text-lg">Perímetro</h3>
+        <div className="flex flex-col items-start justify-center gap-2">
+          <label htmlFor="catetoAd">Digite o cateto adjacente</label>
+          <input 
+            className="bg-slate-600 rounded-lg p-1"
+            id="catetoAd" 
+            type="number" 
+            placeholder="Cateto Adjacente" 
+            required
+            onChange={e => setValoresArea({
+              ...valoresArea,
+              base: Number(e.target.value)
+            })}
+          />
+        </div>
+        <div className="flex flex-col items-start justify-center gap-2">
+          <label htmlFor="catetoOp">Digite o cateto oposto</label>
+          <input 
+            className="bg-slate-600 rounded-lg p-1" 
+            id="catetoOp" 
+            type="number" 
+            placeholder="Cateto Oposto" 
+            required
+            onChange={e => setValoresArea({
+              ...valoresArea,
+              altura: Number(e.target.value)
+            })}
+          />
+        </div>
+        <div className="flex flex-col items-start justify-center gap-2">
+          <label htmlFor="hipotenusa">Digite a hipotenusa</label>
+          <input 
+            className="bg-slate-600 rounded-lg p-1" 
+            id="hipotenusa" 
+            type="number" 
+            placeholder="Hipotenusa" 
+            required
+            onChange={e => setValoresArea({
+              ...valoresArea,
+              hipo: Number(e.target.value)
+            })}
+          />
+        </div>
+        <button type="submit" className="bg-slate-500 hover:bg-slate-400 p-1 rounded-lg">Calcular perímetro</button>
+        { resultados.perimetro && <h4>{resultados.perimetro}</h4> }
       </form>
     </div>
   )
