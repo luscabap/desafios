29ddfd66135d766394fa1c04048846b2728a0268
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-19 17:01:49 -0300

    feat: DesafioPage

--- src/pages/DesafioPage/index.tsx
+++ src/pages/DesafioPage/index.tsx
@@ -15,6 +15,7 @@ export const DesafioPage = () => {
     <AnimatePresence>
       <motion.div className="my-5 flex flex-col items-center justify-center gap-20 w-1/2">
           <TitleSection text={desafio.nome} contrast={true} size="large"/>
+          <h3 className="self-start">Funcionamento</h3>
           <motion.div 
             className="w-full"
             initial={{ x: -150 }}
@@ -26,7 +27,7 @@ export const DesafioPage = () => {
           </motion.div>
 
           <motion.button 
-            className="text-slate-500 flex items-center justify-center w-full" onClick={() => navigate("/")}
+            className="text-slate-500 items-center justify-center flex" onClick={() => navigate("/")}
             initial={{ x: 100 }}
             whileInView={{ x: 0 }}
           >
