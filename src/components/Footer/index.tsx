dac5bca4b28c0596314247ace86e3a3b67f6a912
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-19 18:53:02 -0300

    feat: new Componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full bg-zinc-900 py-4 text-center">
+      <p>Todos os direitos reservados | Site desenvolvido por <a href="https://portfolio-luscabap.vercel.app/" target="_blank" className="cursor-crosshair underline">Lucas Baptista</a> &copy;</p>
+    </footer>
+  )
+}
--- src/pages/BasePage/index.tsx
+++ src/pages/BasePage/index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import { Header } from "../../components/Header"
+import { Footer } from "../../components/Footer"
 
 export const BasePage = () => {
   return (
-    <div className="bg-zinc-800 text-white font-roboto min-h-screen flex flex-col items-center justify-start">
+    <div className="bg-zinc-800 text-white font-roboto min-h-svh flex flex-col items-center justify-between">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   )
 }
