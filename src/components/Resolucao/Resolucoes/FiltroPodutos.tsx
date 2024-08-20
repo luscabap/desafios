d1d77110cea281aa09afe6e62d9ec66c123adea0
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-20 18:46:07 -0300

    feat: FiltroProduto

--- src/components/Resolucao/Resolucoes/FiltroPodutos.tsx
+++ src/components/Resolucao/Resolucoes/FiltroPodutos.tsx
@@ -1,32 +1,72 @@
 import { useEffect, useState } from "react";
 import { Product } from "../../../types/TProducts";
-import { TApiResponse } from "../../../types/TApiResponse";
+import fetchProducts from "../../../utils/fetchProducts";
+import { Produto } from "../../Produto";
+
+type OrdemType = "padrao" | "prc-asc" | "prc-desc" | "qtd-asc" | "qtd-desc";
 
 const FiltroProdutos = () => {
-  const [data, setData] = useState<null | Product[]>(null);
-  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState<undefined | Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [ordemProdutos, setOrdemProdutos] = useState<OrdemType>("padrao");
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await fetch("https://dummyjson.com/products");
-      const info: TApiResponse = await res.json()
-      setData(info.products)
-      setIsLoading(false)
-    }
-    fetchProducts()
-  }, [])
+    const fetchData = async () => {
+      const dataApi = await fetchProducts();
+      setData(dataApi.products);
+      setIsLoading(false);
+    };
 
-  if (isLoading){
-    return <div>CARREGANDO...</div>
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>CARREGANDO...</div>;
   }
 
+  const onChangeOrder = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setOrdemProdutos(e.target.value as OrdemType);
+
+    if (!data) return [];
+
+    const filteredData = data.sort((a, b) => {
+      switch (ordemProdutos) {
+        case "padrao":
+          return (a.price - b.price);
+        case "prc-asc":
+          return (b.price - a.price);
+        case "prc-desc":
+          return (a.price - b.price);
+        case "qtd-asc":
+          return (a.stock - b.stock);
+        case "qtd-desc":
+          return (b.stock - a.stock);
+        default:
+          break;
+      }
+    })
+
+    setData(filteredData)
+  };
+  
+
   return (
-    <div>
-      { data && (
-        data.map(item => <h4 key={item.id}>{item.brand}</h4>)
-      )}
+    <div className="flex flex-col items-center justify-center gap-8">
+      <select onChange={onChangeOrder} className="bg-slate-400 self-start p-2 rounded-lg">
+        <option value="padrao">Selecione uma opção</option>
+        <option value="prc-asc">Preço - Ordem crescente</option>
+        <option value="prc-desc">Preço - Ordem decrescente</option>
+        <option value="qtd-asc">Quantidade - Ordem crescente</option>
+        <option value="qtd-desc">Quantidade - Ordem decrescente</option>
+      </select>
+      <div className="flex flex-wrap items-center justify-center gap-8">
+        {data &&
+          data?.map((item, i) => (
+            <Produto produto={item} key={item.id} num={i}/>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default FiltroProdutos;
