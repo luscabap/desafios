355e635d486f7d9405a7060f035bdb2d38ce25ba
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-20 18:45:57 -0300

    feat: fetchApi with axios

--- src/utils/fetchProducts.ts
+++ src/utils/fetchProducts.ts
@@ -1,17 +1,9 @@
-import axios from "axios"
-import { useState } from "react";
-import { Product } from "../types/TProducts";
+import axios from "axios";
 import { TApiResponse } from "../types/TApiResponse";
 
-const useFetchProducts = async () => {
-  const [data, setData] = useState<Product[] | null>(null);
+const fetchProducts = async () => {
   const res = await axios.get<TApiResponse>("https://dummyjson.com/products");
-  console.log("res", res)
-  setData(res.data.products)
+  return res.data;
+};
 
-  return (
-    data
-  )
-}
-
-export default useFetchProducts;
+export default fetchProducts;
