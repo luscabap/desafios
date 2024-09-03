b8449dd35f97298da6f26870be49fdc9d25495eb
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-09-03 20:14:06 -0300

    feat: validating error

--- src/components/Resolucao/Resolucoes/CRUD.tsx
+++ src/components/Resolucao/Resolucoes/CRUD.tsx
@@ -1,7 +1,10 @@
 import { FormEvent, useState } from "react";
 import { CardMembro } from "../../CardMembro";
 
-type especialidades = "Desenvolvedor Front-End" | "Desenvolvedor Back-End" | "Administrador de Banco de Dados"
+type especialidades =
+  | "Desenvolvedor Front-End"
+  | "Desenvolvedor Back-End"
+  | "Administrador de Banco de Dados";
 
 enum EnumEspecialidades {
   devfront = "Desenvolvedor Front-End",
@@ -12,7 +15,7 @@ enum EnumEspecialidades {
 type TItem = {
   name: string;
   email: string;
-  especialidade: especialidades | null;
+  especialidade: especialidades | "";
   id: string;
 };
 
@@ -33,7 +36,7 @@ const Crud = () => {
   const [valores, setValores] = useState<TItem>({
     email: "",
     name: "",
-    especialidade: null,
+    especialidade: "",
     id: "",
   });
 
@@ -42,37 +45,45 @@ const Crud = () => {
     const itemCriado = { ...valores, id: gerarId() };
     const novoArray = [...itens, itemCriado];
     setItens(novoArray);
-    limpaCampos();
+    limparElementos();
   };
 
-  const limpaCampos = () => {
+  const limparElementos = () => {
     setValores({
       email: "",
       name: "",
-      especialidade: null,
+      especialidade: "",
       id: "",
     });
-  };
+  }
 
   const changeElement = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const valorEnum = e.target.value as keyof typeof EnumEspecialidades;
-    const especialidade = EnumEspecialidades[valorEnum]
+    const especialidade = EnumEspecialidades[valorEnum];
+
+    console.log(especialidade);
+    
 
     setValores({
       ...valores,
-      especialidade
-    })
-  }
+      especialidade,
+    });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center gap-20">
       <h3 className="text-lg text-slate-600">Criar cadastro de TechPeople</h3>
-      <form className="text-white flex flex-col items-start justify-start gap-8" onSubmit={createElement}>
+      <form
+        className="text-white flex flex-col items-start justify-start gap-8"
+        onSubmit={createElement}
+      >
         <div className="flex items-center justify-center gap-4">
           <label>Digite o nome</label>
           <input
             type="text"
             required
             placeholder="Nome"
+            value={valores.name}
             className="p-1 rounded-lg bg-slate-700"
             onChange={(e) =>
               setValores({
@@ -89,6 +100,7 @@ const Crud = () => {
             required
             placeholder="Email"
             className="p-1 rounded-lg bg-slate-700"
+            value={valores.email}
             onChange={(e) =>
               setValores({
                 ...valores,
@@ -99,7 +111,7 @@ const Crud = () => {
         </div>
         <div className="flex flex-col items-start justify-start w-full">
           <h4>Selecione o cargo</h4>
-          <select 
+          <select
             className="p-1 rounded-lg bg-slate-700 w-full"
             onChange={changeElement}
             required
@@ -110,13 +122,18 @@ const Crud = () => {
             <option value="dba">Administrador de Banco de Dados</option>
           </select>
         </div>
-        <button type="submit" className="self-center bg-slate-500 p-2 rounded-lg text-black hover:bg-slate-300">Criar</button>
+        <button
+          type="submit"
+          className="self-center bg-slate-500 p-2 rounded-lg text-black hover:bg-slate-300"
+        >
+          Criar
+        </button>
       </form>
       <div className="flex flex-col items-center justify-center gap-8">
         <h4>Membros cadastrados</h4>
         <div className="flex flex-wrap gap-8 max-w-96">
-          {itens?.map(item => (
-            <CardMembro 
+          {itens?.map((item) => (
+            <CardMembro
               email={item.email}
               funcao={item.especialidade!}
               nome={item.name}
